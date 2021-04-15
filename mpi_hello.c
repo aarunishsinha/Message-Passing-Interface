54d4402645df831b70c361fb27afe5eaa5e81733
Aarunish Sinha <58175652+aarunishsinha@users.noreply.github.com>
2021-04-15 18:33:43 +0530

    added gitignore

--- mpi_hello.c
+++ mpi_hello.c
@@ -4,12 +4,12 @@
 
 const int MAX_STRING = 100;
 
-int main(void){
+int main(int argc, char* argv[]){
   char greeting[MAX_STRING];
   int comm_sz;
   int my_rank;
 
-  MPI_Init(NULL,NULL);
+  MPI_Init(&argc,&argv);
   MPI_Comm_size(MPI_COMM_WORLD,&comm_sz);
   MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
 
